98306736f572fb3b1f2e0f1cfa7ecc8f5e6d8396
Petro Rovenskyy <petro.rovenskyy@gmail.com>
2015-07-25 11:29:20 +0300

    Initial commit, added Podfile, added .gitignore

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'Alamofire', '~> 1.2.3' # network
+pod 'EasyMapping', '~> 0.15.0' # Objective-C pod
+pod 'SwiftyJSON', '~> 2.2.0' # serializations
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 3
+
+// EasyMapping
+#define COCOAPODS_POD_AVAILABLE_EasyMapping
+#define COCOAPODS_VERSION_MAJOR_EasyMapping 0
+#define COCOAPODS_VERSION_MINOR_EasyMapping 15
+#define COCOAPODS_VERSION_PATCH_EasyMapping 1
+
+// EasyMapping/Core
+#define COCOAPODS_POD_AVAILABLE_EasyMapping_Core
+#define COCOAPODS_VERSION_MAJOR_EasyMapping_Core 0
+#define COCOAPODS_VERSION_MINOR_EasyMapping_Core 15
+#define COCOAPODS_VERSION_PATCH_EasyMapping_Core 1
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
+
